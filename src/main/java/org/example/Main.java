de80dfcf2fc99a3bfbf8a3e0007cb8b498d65a6f
matvejIDer <derebezovm@mail.ru>
2025-03-14 17:58:31 +0700

    добавил UPDATE и DELETE

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -15,13 +15,35 @@ public class Main {
 
         try (Connection connect = dataBaseConnection.connect()) {
             BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-            System.out.print("Введите название задачи: ");
-            String title = bf.readLine();
-            System.out.print("Введите описание задачи: ");
-            String description = bf.readLine();
-            LocalDateTime dateTime = LocalDateTime.now();
-            insertTask(title, description, dateTime, connect);
+            System.out.println("Выберите действие:");
+            System.out.println("1. Добавить задачу");
+            System.out.println("2. Удалить задачу");
+            System.out.println("3. Изменить статус задачи на DONE");
+            String choice = bf.readLine();
 
+            switch (choice) {
+                case "1":
+                    System.out.print("Введите название задачи: ");
+                    String title = bf.readLine();
+                    System.out.print("Введите описание задачи: ");
+                    String description = bf.readLine();
+                    LocalDateTime dateTime = LocalDateTime.now();
+                    insertTask(title, description, dateTime, connect);
+                    break;
+                case "2":
+                    System.out.print("Введите id: ");
+                    int deleteId = Integer.parseInt(bf.readLine());
+                    deleteTask(deleteId, connect);
+                    break;
+                case "3":
+                    System.out.print("Введите ID задачи для изменения статуса: ");
+                    int updateId = Integer.parseInt(bf.readLine());
+                    updateTaskStatus(updateId, connect);
+                    break;
+
+                default:
+                    System.out.println("Некорректный выбор");
+            }
         } catch (SQLException | IOException e) {
             System.out.println("Ошибка: " + e.getMessage());
         }
@@ -36,6 +58,29 @@ public class Main {
             preparedStatement.setTimestamp(4, Timestamp.valueOf(dateTime));
             int rowsAffected = preparedStatement.executeUpdate();
             System.out.println(rowsAffected + " вставлено строк");
+        }
+    }
+
+    public static void deleteTask(int id, Connection connect) throws SQLException {
+        String sql = "DELETE FROM test WHERE id = ?;";
+        try (PreparedStatement preparedStatement = connect.prepareStatement(sql)){
+            preparedStatement.setInt(1, id);
+            int rowsAffected = preparedStatement.executeUpdate();
+            System.out.println(rowsAffected + " удалено строк");
+        }
+    }
+
+    public static void updateTaskStatus(int id, Connection connect) throws SQLException {
+        String sql = "UPDATE test SET status = ? WHERE id = ? AND status = 'TODO';";
+        try (PreparedStatement preparedStatement = connect.prepareStatement(sql)){
+            preparedStatement.setString(1, "DONE");
+            preparedStatement.setInt(2, id);
+            int rowsAffected = preparedStatement.executeUpdate();
+            if (rowsAffected > 0) {
+                System.out.println("Статус задачи обновлён на 'DONE'");
+            } else {
+                System.out.println("Задача не найдена или уже выполнена");
+            }
         }
     }
 }
